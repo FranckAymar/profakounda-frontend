cf0ca13213f766d69405c4cd6ac05a1bdf4ce3c8
FranckAymar <franck.braffouo@gmail.com>
2020-03-08 12:04:48 +0000

    configuration du projet, ajout des librairies, insertion du template admin

---
+++ src/app/app.module.ts
@@ -0,0 +1,44 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContentAdminComponent } from './content-admin/content-admin.component';
+import { RouterModule, Routes} from '@angular/router'
+import { DashboardHomeComponent } from './dashboard-home/dashboard-home.component';
+import { ClasseComponent } from './admin/classe/classe.component';
+import { CycleComponent } from './admin/cycle/cycle.component';
+import { FiliereComponent } from './admin/filiere/filiere.component';
+
+
+const appRouter : Routes = [
+  {path: 'admin/dashboard-home', component  : DashboardHomeComponent},
+  {path: 'admin/classe', component  : ClasseComponent},
+  {path: 'admin/cycle', component  : CycleComponent},
+  {path: 'admin/filiere', component  : FiliereComponent}
+
+  
+] ;
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    SidebarComponent,
+    FooterComponent,
+    ContentAdminComponent,
+     DashboardHomeComponent,
+     ClasseComponent,
+     CycleComponent,
+     FiliereComponent
+  ],
+  imports: [
+    BrowserModule, RouterModule.forRoot(appRouter)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

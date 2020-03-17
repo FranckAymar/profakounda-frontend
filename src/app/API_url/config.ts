e7ca9b05037af4710b06d9116b648fc79b478f4a
FranckAymar <franck.braffouo@gmail.com>
2020-03-17 23:30:18 +0000

    Affichage des cycles et des filières

--- src/app/API_url/config.ts
+++ src/app/API_url/config.ts
@@ -5,5 +5,11 @@ export const URL = {
     // Cette classe contiendra les différents URLs des services fournis oar le serveur
 
     //URL pour les classes
-    recupererClasse : consts.host+ consts.nameProject +'recupererClasse'    
+    recupererClasse : consts.host+ consts.nameProject +'recupererClasse'    ,
+
+    //Recuperer Filiere
+    recupererFiliere :  consts.host+ consts.nameProject +'filieres'   ,
+    
+    //RecupererCycle
+    recupererCycle : consts.host+ consts.nameProject +'cycles'   
 }

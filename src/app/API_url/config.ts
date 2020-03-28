3d07e4c9de8bf7ed11c8fb8e374ae3f29a70e880
FranckAymar <franck.braffouo@gmail.com>
2020-03-28 18:16:01 +0000

    reorganisation liens

--- src/app/API_url/config.ts
+++ src/app/API_url/config.ts
@@ -7,37 +7,37 @@ export const URL = {
     //URL pour les classes
 
 
-    recupererClasse : consts.host+ consts.nameProject +'admin/recupererClasse' ,
+    recupererClasse : consts.host+ consts.nameProject +'recupererClasse' ,
     enregistrerClasse : consts.host+ consts.nameProject +'admin/enregistrerClasse'   ,
     supprimerClasse : consts.host+ consts.nameProject +'admin/supprimerClasse' ,
     modifierClassse :  consts.host+ consts.nameProject +'admin/modifierClasse' ,
     //URL des Filieres
-    recupererFiliere :  consts.host+consts.nameProject+'admin/filieres' ,
+    recupererFiliere :  consts.host+consts.nameProject+'filieres' ,
     enregistrerFiliere : consts.host+ consts.nameProject +'admin/enregistrerFiliere',
     modifierFiliere:  consts.host+ consts.nameProject +'admin/modifierFiliere' ,
     supprimerFiliere:  consts.host+ consts.nameProject +'admin/supprimerFiliere' ,
     
     //Cycle
-    recupererCycle : consts.host+ consts.nameProject +'admin/cycles'   ,
+    recupererCycle : consts.host+ consts.nameProject +'cycles'   ,
     enregistrerCycle:consts.host+ consts.nameProject +'admin/enregistrerCycle',
     supprimerCycle:consts.host+ consts.nameProject +'admin/deleteCycle',
     retrouverCycleParId:consts.host+ consts.nameProject +'admin/getCycle',
     modifierCycle:consts.host+ consts.nameProject +'admin/updateCycle',
 
     //Forfait
-    recupererForfaits : consts.host+ consts.nameProject +'admin/recupererForfait',
+    recupererForfaits : consts.host+ consts.nameProject +'recupererForfait',
     enregistrerForfait : consts.host+ consts.nameProject +'admin/enregistrerForfait' ,
     supprimerForfait : consts.host+ consts.nameProject +'admin/supprimerForfait',
     modifierForfait :  consts.host+ consts.nameProject +'admin/modifierForfait',
     //Niveau
-    recupererNiveau : consts.host+ consts.nameProject +'admin/niveaux'   ,
+    recupererNiveau : consts.host+ consts.nameProject +'niveaux'   ,
     enregistrerNiveau:consts.host+ consts.nameProject +'admin/enregistrerNiveau',
     supprimerNiveau:consts.host+ consts.nameProject +'admin/deleteNiveau',
     retrouverNiveauParId:consts.host+ consts.nameProject +'admin/getNiveau',
     modifierNiveau:consts.host+ consts.nameProject +'admin/updateNiveau',
     //Particulier
     enregistrerParticulier : consts.host+ consts.nameProject +'enregistrerParticulier',
-    modifierParticulier : consts.host+ consts.nameProject +'modifierParticulier',
+    modifierParticulier : consts.host+ consts.nameProject +'particulier/modifierParticulier',
     rechercherParticulier : consts.host+ consts.nameProject +'rechercherParticulierParUsername',
     getPhoto : consts.host+ consts.nameProject +'photoParticulier',
     //SIGN IN

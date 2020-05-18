72238b3e85e73f982d4be17fb59b6df1880af70d
sangare <sangareadama7930@gmail.com>
2020-05-18 00:10:51 +0000

    simple commit

--- src/app/admin/admin-common-course/admin-common-course.component.ts
+++ src/app/admin/admin-common-course/admin-common-course.component.ts
@@ -1,5 +1,6 @@
 import { CoursCommunAdminService } from './../services/cours-commun-admin.service';
 import { Component, OnInit } from '@angular/core';
+import { CauserefusService } from '../services/causerefus.service';
 
 @Component({
   selector: 'app-admin-common-course',
@@ -8,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminCommonCourseComponent implements OnInit {
 
-  constructor(private coursCommunAdminService : CoursCommunAdminService) { }
+  constructor(private coursCommunAdminService : CoursCommunAdminService, private refusService:CauserefusService) { }
 
   courCommuns= [];
+
+  idPropositionCourante : number ;
+  causeRefusId : number;
+  causeRefusTab = [];
+
 
   ngOnInit() {
 
@@ -21,7 +27,6 @@ export class AdminCommonCourseComponent implements OnInit {
 
     
     this.coursCommunAdminService.fetchCoursCommun().subscribe(
-
 
       (resp) => {
         this.courCommuns = resp ;
@@ -36,4 +41,85 @@ export class AdminCommonCourseComponent implements OnInit {
 
     )
   }
+
+  onAccorderMiseEnLigne(idProposition){
+    this.coursCommunAdminService.mettreEnLigne(idProposition).subscribe(
+
+
+      (resp)=>{
+        
+        alert("mise en ligne accordé") ;
+        //this.onFetchPropositionFormation(this.critereFiltre)
+        this.onFetchCoursCommuns();
+
+      },
+      (error)=>{
+          console.log(error);
+          
+      }
+      
+
+    )
+  }
+
+  prepareToRefusEnLigne(idProposition){
+    this.idPropositionCourante = idProposition ;
+  }
+
+  onRefuserMiseEnLigne(){
+
+    this.coursCommunAdminService.refuserMiseEnLigne(this.idPropositionCourante, this.causeRefusId).subscribe(
+
+
+      (resp)=>{
+        alert("Refus validé") ;
+        document.getElementById('closeModalChoixRefus').click() ;
+        //this.onFetchPropositionFormation(this.critereFiltre) ;
+        this.onFetchCoursCommuns();
+      },
+      (error)=>{
+
+        console.log(error);
+        
+      }
+      
+    )
+  }
+
+
+  onFetchCauseRefus(){
+
+    this.refusService.fetchRefus().subscribe(
+
+      (resp)=>{
+
+        this.causeRefusTab = resp ;
+      },
+
+      (error)=>{
+
+        console.log(error);
+        
+      }
+    )
+  }
+
+
+  supprimerMiseEnLigne(id){
+
+    confirm('Etes vous sur de cette action ?') ;
+
+    this.coursCommunAdminService.supprimerMiseEnLigne(id).subscribe(
+    
+      (response)=>{ 
+        //this.onFetchPropositionFormation(this.critereFiltre) ;
+        this.onFetchCoursCommuns();
+      },
+      (error)=>{
+        console.log("Error de suppression: "+error);
+      }
+    )
+
+  }
+
 }

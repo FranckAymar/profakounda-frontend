17810a262885ac9cca4f661c32d1b1ae32d02e9c
Franck Aymar <franck.braffouo@gmail.com>
2020-05-21 22:43:46 +0000

    bug lien ok

--- src/app/customers/services/cours-commun-service.service.ts
+++ src/app/customers/services/cours-commun-service.service.ts
@@ -95,22 +95,9 @@ export class CoursCommunService {
     return this.http.get(URL.recupererCoursCommunsInscritsParticulier);
    }
 
-
-
-<<<<<<< HEAD
    fermerInscriptionCoursCommun(id){
     
     return this.http.post(URL.fermerInscriptionCoursCommun, id);
-=======
-   fermerInscriptionCoursCommun(id : number){
-
-    let data = {
-      idCoursCommun : id
-    }
-    console.log(data);
-    
-    return this.http.post(URL.fermerInscriptionCoursCommun+ '/'+ id , null);
->>>>>>> 5adc74f4b099cd382a85ae4e8e5b654552d79ef4
 
    }
 

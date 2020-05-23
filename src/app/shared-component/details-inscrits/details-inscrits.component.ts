bba9a9094a548eea40634856098f891d34a6eea3
Franck Aymar <franck.braffouo@gmail.com>
2020-05-23 20:54:03 +0000

    before merge

--- src/app/shared-component/details-inscrits/details-inscrits.component.ts
+++ src/app/shared-component/details-inscrits/details-inscrits.component.ts
@@ -1,10 +1,10 @@
-import { PdfMakeWrapper, Toc, Txt, TocItem, Table, Cell } from 'pdfmake-wrapper';
+import { PropostionFormationsAdminService } from './../../admin/services/propostion-formations-admin.service';
+import { PdfMakeWrapper, Toc, Txt, TocItem, Table, Cell, Img, Stack, Columns } from 'pdfmake-wrapper';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import { Component, OnInit, Inject } from '@angular/core';
 import pdfFonts from "pdfmake/build/vfs_fonts"; // fonts provided for pdfmake
-import pdfMake from 'pdfmake/build/pdfmake';
 import { DatePipe } from '@angular/common';
-pdfMake.vfs = pdfFonts.pdfMake.vfs;
+import localeFr from '@angular/common/locales/fr';
 @Component({
   selector: 'app-details-inscrits',
   templateUrl: './details-inscrits.component.html',
@@ -14,7 +14,7 @@ export class DetailsInscritsComponent implements OnInit {
 
 
   headersTable = ['Nom', 'Prénoms', 'Date Inscription'];
-  body = [] ;
+  body = [];
 
 
 
@@ -37,35 +37,57 @@ export class DetailsInscritsComponent implements OnInit {
     let line;
     dataReceived.forEach(element => {
 
-      line = [element.nom, element.prenoms, this.datePipe.transform(element.dateInscription,'dd-MMMM-yyyy')];
+      line = [element.nom, element.prenoms, this.datePipe.transform(element.dateInscription, 'dd-MMMM-yyyy')];
       this.body.push(line);
     });
 
   }
 
 
-  generatePDFListInscrit(){
-
+  generatePDFListInscrit() {
 
     PdfMakeWrapper.setFonts(pdfFonts);
-
+   
+  
     const pdf = new PdfMakeWrapper();
+
+  
+
+  //  pdf.add(new Img('../../../assets/images/icon/awa/Stylo_Noir_SF.png').build());
+
+   
+
+
+    new Img('../../../assets/images/logo.png').width(150).margin(40).build().then( img => {
+    pdf.add( img);
+    
 
     this.createBodyOfTable(this.dataReceived.inscrit);
 
-    if(!this.dataReceived.details.coursCommun){
-      
-      pdf.add(new Txt('Niveau : ' + this.dataReceived.details.niveau.libelle).end);
+    if (!this.dataReceived.details.coursCommun) {
+      pdf.add(new Txt('Noveau : ' + this.dataReceived.details.niveau.libelle).end);
       pdf.add(new Txt('Filière : ' + this.dataReceived.details.filiere).end);
+     // pdf.add(new Txt('Formations : ' + this.dataReceived.details.filiere).end);
 
+    }else{
+      pdf.add(new Txt('Organisation : ' + this.dataReceived.details.organisation.libelle).bold().end);
+      pdf.add(new Txt('Titre : ' + this.dataReceived.details.titre).end);
+  
     }
 
-    pdf.add(new Txt('Liste des inscrits').end);
-    pdf.add(new Table(this.body).alignment('center').widths([ 150, 150, 150 ]).end) ;
+    pdf.add(new Txt('Liste des inscrits').alignment('center').decoration('underline').margin(20).bold().end);
+    pdf.add(new Table(this.body).alignment('center').widths([150, 150, 150]).end);  
 
+    pdf.footer('Imprimé le : ' + new Date())
 
     pdf.create().open();
 
+      });
+
+   
   }
+
+
+
 
 }

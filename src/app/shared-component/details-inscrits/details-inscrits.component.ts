09a8dae9b64d243cb286dbe97ae1e3960091767e
Franck Aymar <franck.braffouo@gmail.com>
2020-05-30 23:30:32 +0000

    correction bug front mobile

--- src/app/shared-component/details-inscrits/details-inscrits.component.ts
+++ src/app/shared-component/details-inscrits/details-inscrits.component.ts
@@ -91,6 +91,7 @@ export class DetailsInscritsComponent implements OnInit {
 
     pdf.footer('Imprimé le : ' + new Date().toDateString())
 
+    pdf.create().download('Liste_profAkounda_'+ this.dataReceived.details.cours.organisation.libelle + "_"+ new Date().getDate().toString()  );
     pdf.create().open();
 
       });

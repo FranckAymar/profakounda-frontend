aaecb27a042d2b7079ea935815558f294a5bf200
Franck Aymar <franck.braffouo@gmail.com>
2020-06-05 18:06:10 +0000

    before commit

--- src/app/API_url/const.ts
+++ src/app/API_url/const.ts
@@ -1,6 +1,6 @@
 
 export const consts = {
-    //host: "https://api.profakounda.com/",
+   // host: "https://api.profakounda.com/",
     host : "http://localhost:8080/",
     nameProject: "prof-akounda-web/",
 

0d31468ead63b9e5df8570ad15bdf07083aad567
Franck Aymar <franck.braffouo@gmail.com>
2020-07-10 08:29:36 +0000

    quelques ajustements sur l'affichage

--- src/app/API_url/const.ts
+++ src/app/API_url/const.ts
@@ -2,6 +2,6 @@
 export const consts = {
 
     host: "https://api.profakounda.com/",
-   //host : "http://localhost:8080/",
+//host : "http://localhost:8080/",
     nameProject: "prof-akounda-web/",
 } 

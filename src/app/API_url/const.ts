37804d11d5aa6d47f230efc3f1cb517e988cf5d1
Tanfolo <tanfolojoel@gmail.com>
2020-07-23 14:52:32 +0000

    Afficher le text de coût négociable

--- src/app/API_url/const.ts
+++ src/app/API_url/const.ts
@@ -1,13 +1,8 @@
 
 export const consts = {
 
-<<<<<<< HEAD
     //host: "https://api.profakounda.com/",
 host : "http://localhost:8080/",
-=======
-    host: "https://api.profakounda.com/",
-    //host : "http://localhost:8080/",
->>>>>>> 97a73c46994a56197289258c706e4332f00c7ae3
     nameProject: "prof-akounda-web/",
 
 } 

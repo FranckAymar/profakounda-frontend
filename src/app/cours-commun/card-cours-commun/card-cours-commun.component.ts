5870836bcc7c0010a05e0478179a0a87c3eadacc
sangare <sangareadama7930@gmail.com>
2020-09-13 22:46:04 +0000

    recuperation

--- src/app/cours-commun/card-cours-commun/card-cours-commun.component.ts
+++ src/app/cours-commun/card-cours-commun/card-cours-commun.component.ts
@@ -11,12 +11,44 @@ export class CardCoursCommunComponent implements OnInit {
 
   urlServer = URL.getAffichePubCoursCommun;
 
-
+ 
   @Input() cours;
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() {      
+   
   }
 
+  chargerTwitter(){
+    alert("super")
+    var url="https://profAkounda.com/courscommun/details/"+this.cours.coursCommun.id;
+    var siteUrl="https://twitter.com/intent/tweet?text=Hello ! voici%20mon%20cours%20commun  " +encodeURIComponent(url);
+    
+    window.open(siteUrl,"partage");
+  }
+
+  chargerFace(){
+    alert("super")
+    var url="https://profAkounda.com/courscommun/details/"+this.cours.coursCommun.id;
+    var siteUrl="https://www.facebook.com/sharer/sharer.php?u=" +encodeURIComponent(url);
+    
+    window.open(siteUrl,"partage");
+  }
+
+  chargerLink(){ 
+    alert("super")
+    var url="https://profAkounda.com/courscommun/details/"+this.cours.coursCommun.id;
+    var siteUrl="https://www.linkedin.com/shareArticle?url=" +encodeURIComponent(url);
+    
+    window.open(siteUrl,"partage");
+  }
+
+  chargerInsta(){
+    alert("super")
+    var url="https://profAkounda.com/courscommun/details/"+this.cours.coursCommun.id;
+    var siteUrl="https://www.whatsapp.com/faq/en/general";
+    
+    window.open(siteUrl,"partage");
+  }
 }

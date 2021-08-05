655a0087eb9226c6450faedb7c1e48d3b8168484
SANGARE ADAMA <sangareadama7930@gmail.com>
2021-08-05 17:01:32 +0000

    test

--- src/app/API_url/const.ts
+++ src/app/API_url/const.ts
@@ -1,8 +1,6 @@
 
 export const consts = {
-    host: "https://api.profakounda.com/",
-    //host : "http://localhost:8180/",
-
-    
+   // host: "https://api.profakounda.com/",
+    host : "http://localhost:8180/",
     nameProject: "prof-akounda-web/",
 } 

39629244df8593f08fef809a79a495bb52ff7226
Franck Braffouo <franckbraffouo@macbook-air-de-franck.home>
2022-02-12 12:31:34 +0100

    feat() : migrating to angular 9

--- src/app/formations/details-annonce/details-annonce.component.ts
+++ src/app/formations/details-annonce/details-annonce.component.ts
@@ -26,10 +26,10 @@ declare var $: any;
 export class DetailsAnnonceComponent implements OnInit {
 
   //ViewChild
-  @ViewChild('openModalPayment',  {static: false}) openModalPayment: ElementRef<HTMLElement>;
-  @ViewChild('closeModalPayment',  {static: false}) closeModalPayment: ElementRef<HTMLElement>;
-  @ViewChild('openParticulierDetails',  {static: false}) openParticulierDetails: ElementRef<HTMLElement>;
-  @ViewChild('goToSignin',  {static: false}) goToSignin: ElementRef<HTMLElement>;
+  @ViewChild('openModalPayment') openModalPayment: ElementRef<HTMLElement>;
+  @ViewChild('closeModalPayment') closeModalPayment: ElementRef<HTMLElement>;
+  @ViewChild('openParticulierDetails') openParticulierDetails: ElementRef<HTMLElement>;
+  @ViewChild('goToSignin') goToSignin: ElementRef<HTMLElement>;
 
   
   //Variable for condition
